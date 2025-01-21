0fc3a2a7fb427acd37b59e50a160b8cc3ba5428f
mathsphile <ndas20997@gmail.com>
2025-01-21 22:10:15 +0530

    Reverse a linkedList

---
+++ Day20.java
@@ -0,0 +1,43 @@
+static Node reverseKGroup(Node head, int k) {
+    if (head == null) {
+        return head;
+    }
+
+    Node curr = head;
+    Node newHead = null;
+    Node tail = null;
+
+    while (curr != null) {
+        Node groupHead = curr;
+        Node prev = null;
+        Node nextNode = null;
+        int count = 0;
+
+        // Reverse the nodes in the current group
+        while (curr != null && count < k) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+            count++;
+        }
+
+        // If newHead is null, set it to the
+        // last node of the first group
+        if (newHead == null) {
+            newHead = prev;
+        }
+
+        // Connect the previous group to the
+        // current reversed group
+        if (tail != null) {
+            tail.next = prev;
+        }
+
+        // Move tail to the end of the
+        // reversed group
+        tail = groupHead;
+    }
+
+    return newHead;
+}
