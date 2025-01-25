7b0542763f509aa70fc3eb1fd0384dfb72ea9458
mathsphile <ndas20997@gmail.com>
2025-01-25 22:17:29 +0530

    Linked list
     Solved the challenge to find the first node of a loop in a singly linked list using Floyd’s Cycle Detection algorithm.

---
+++ Day22.java
@@ -0,0 +1,48 @@
+// Given a head of the singly linked list. If a loop is present in the list then return the first node of the loop else return NULL.
+
+// Custom Input format:
+// A head of a singly linked list and a pos (1-based index) which denotes the position of the node to which the last node points to. If pos = 0, it means the last node points to null, indicating there is no loop.
+class Solution {
+
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int[] numsSorted = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            numsSorted[i] = nums[i];
+        Arrays.sort(numsSorted);
+
+        int currGroup = 0;
+        HashMap<Integer, Integer> numToGroup = new HashMap<>();
+        numToGroup.put(numsSorted[0], currGroup);
+
+        HashMap<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+        groupToList.put(
+                currGroup,
+                new LinkedList<Integer>(Arrays.asList(numsSorted[0])));
+
+        for (int i = 1; i < nums.length; i++) {
+            if (Math.abs(numsSorted[i] - numsSorted[i - 1]) > limit) {
+                // new group
+                currGroup++;
+            }
+
+            // assign current element to group
+            numToGroup.put(numsSorted[i], currGroup);
+
+            // add element to sorted group list
+            if (!groupToList.containsKey(currGroup)) {
+                groupToList.put(currGroup, new LinkedList<Integer>());
+            }
+            groupToList.get(currGroup).add(numsSorted[i]);
+        }
+
+        // iterate through input and overwrite each element with the next element in its
+        // corresponding group
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int group = numToGroup.get(num);
+            nums[i] = groupToList.get(group).pop();
+        }
+
+        return nums;
+    }
+}
