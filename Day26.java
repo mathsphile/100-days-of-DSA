2e9781dd60fc9678d9402e3bd83844320c22530c
mathsphile <ndas20997@gmail.com>
2025-01-29 12:43:35 +0530

    Implement POW
    Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).

---
+++ Day26.java
@@ -0,0 +1,6 @@
+// Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+class Solution {
+    double power(double b, int e) {
+        return Math.pow(b, e);
+    }
+}
